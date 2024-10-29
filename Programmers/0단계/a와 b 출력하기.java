3268d890d2ca8c533d348abf264ab7be789fa4ff
성유철 <tjddbcjf1@naver.com>
2024-10-29 13:45:07 +0900

    [0단계] a와 b 출력하기

---
+++ Programmers/0단계/a와 b 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/* a와 b 출력하기 */
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println("a = "  + a);
+        System.out.println("b = "  + b);
+    }
+}
