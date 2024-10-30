79a1fa953508409098bf76e76e57bb6f42424d26
성유철 <tjddbcjf1@naver.com>
2024-10-30 16:20:06 +0900

    [0단계] 문자열 돌리기 v1

--- Programmers/0단계/문자열 돌리기.java
+++ Programmers/0단계/문자열 돌리기.java
@@ -12,12 +12,20 @@ public class Main {
             System.out.println(str.charAt(i));
         }
 
-        /* 다르게 풀이 */
+        /* .toCharArray() 를 사용한 풀이 */
         String str2 = scan.nextLine();
         char[] ch = str2.toCharArray(); // "abcde" 라는 문자열을 문자 배열로 변환
 
         for (char c : ch) { // 향샹된 for 문을 사용해 ch 의 각 문자를 변수 c에 넣음
             System.out.println(c);
+        }
+
+        /* .split() 를 사용한 풀이 */
+        String str3 = scan.nextLine();
+        String[] res = str3.split(""); // .split() 메서드로 문자열을 한 글자씩 나누어 문자열 배열로 저장
+
+        for(String s : res){ // 향상된 for 문을 사용해 배열 res 의 각 문자를 s 에 할당
+            System.out.println(s);
         }
     }
 }
